68b523d9db4052f3966c01891eb201f53a73c090
davidade300 <iaderinho@icloud.com>
2024-08-28 22:22:56 -0300

    added routers for todos and for auth, created users table, add user_id as fk for todos table

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -11,3 +11,11 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()  # create a object of the database which will then
 # be able to interacti with the tables that we create in the fucture
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,5 +1,18 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+
 from TodoApp.database import Base
+
+
+class Users(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    First_name = Column(String)
+    Last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 
 class Todos(Base):
@@ -10,3 +23,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
