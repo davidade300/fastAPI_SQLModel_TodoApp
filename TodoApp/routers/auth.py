d57966ded810e918b752c6879eb567154323e401
davidade300 <iaderinho@icloud.com>
2024-08-29 20:29:28 -0300

    added create user request for auth router

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,9 +1,30 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
+from TodoApp.models import Users
 
 router = APIRouter()
 
 
-@router.get("/auth/")
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+@router.post("/auth")
+# it's not possible to pass the  CreateUserRequest through model_dump
+# so it's better to just pass it manually
+async def create_users(create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        hashed_password=create_user_request.password,
+        is_active=True,
+    )
+    return create_user_model
